f5fa318e2af0622d0af30f111ebc103ae32f7b55
Upasana Kuar <upasanakuar44@gmail.com>
2022-03-24 03:01:23 +0530

    Add files via upload

---
+++ scripts/nav.js
@@ -0,0 +1,8 @@
+function navbar(){
+    return `<div style="display: grid;grid-template-columns: 70% 10% 10% ; text-align: center;border: 1px solid gray;height:60px;background-color:gray;">
+    <h1>DAILY HUNT</h1>
+    <h3>13-12-2021</h3>
+    <h3>Monday</h3>
+</div>`
+}
+export default navbar;
