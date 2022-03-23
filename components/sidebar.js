f158153663d435670e84df9b31d51e7291b17f2d
Upasana Kuar <upasanakuar44@gmail.com>
2022-03-24 03:00:39 +0530

    Add files via upload

---
+++ components/sidebar.js
@@ -0,0 +1,19 @@
+function sidebar(){
+    return` <div id="container" style="width:100%;height:800px;font-size: 19px;line-height: 35px;">
+    <h3>Join TechCrunch +</h3>
+    <ul style="list-style-type: none;padding-left:20px">
+        <li>Latest</li>
+        <li>TC Sessions: Space</li>
+        <li>Gift Guide</li>
+        <li>Startups</li>
+        <li>Videos</li>
+        <li>Audio</li>
+        <li>Newsletters</li>
+        <li>TechCrunch+</li>
+        <li>Advertise</li>
+        <li>Events</li>
+        <li>More</li>
+    </ul>
+</div>`
+}
+export default sidebar;
